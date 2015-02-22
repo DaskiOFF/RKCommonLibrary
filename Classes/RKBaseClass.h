905e978ae4ba8b1f1d96a35dba52a09e32e34338
Roman Kotov <waydeveloper@gmail.com>
2015-02-23 00:21:31 +0300

    add parsing with JSON

--- Classes/RKBaseClass.h
+++ Classes/RKBaseClass.h
@@ -10,4 +10,40 @@
 
 @interface RKBaseClass : NSObject
 
+/**
+ *  Создание объекта с данными предоставленными в JSON
+ *  Имена !свойств! должны совпадать с ключами в JSON
+ *
+ *  @param  json    данные в формате json
+ *  @return экземпляр объекта с заполненными данными
+ */
++ (instancetype)initWithJson:(NSDictionary*)json;
+
+/**
+ *  Создание массива объектов с данными предоставленными в JSON
+ *  Имена !свойств! должны совпадать с ключами в JSON
+ *
+ *  @param  jsonArray Массив данных в формате json
+ *  @return экземпляр объекта с заполненными данными
+ */
++ (NSArray*)initArrayWithJsonArray:(NSArray*)jsonArray;
+
+/**
+ *  Создание объекта с данными предоставленными в JSON
+ *  Имена !свойств! должны совпадать с ключами в JSON
+ *
+ *  @param  json    данные в формате json
+ *  @return экземпляр объекта с заполненными данными
+ */
+- (instancetype)initWithJson:(NSDictionary*)json;
+
+/**
+ *  Метод возвращающий название класса, объекты которого
+ *  должны быть созданны согласно данным в JSON
+ *
+ *  @param  fieldName   Имя свойства класса типа NSArray
+ *  @return Название класса содержащегося в массиве или nil
+ */
+- (NSString*)classStringForField:(NSString*)fieldName;
+
 @end
--- Classes/RKBaseClass.m
+++ Classes/RKBaseClass.m
@@ -11,6 +11,91 @@
 
 @implementation RKBaseClass
 
+#pragma mark -
+#pragma mark Init
++ (instancetype)initWithJson:(NSDictionary*)json {
+    return [[[self class] alloc] initWithJson:json];
+}
+
++ (NSArray*)initArrayWithJsonArray:(NSArray*)jsonArray {
+    NSMutableArray *resultArray = [NSMutableArray new];
+    
+    for (NSDictionary *json in jsonArray) {
+        [resultArray addObject:[[[self class] alloc] initWithJson:json]];
+    }
+    
+    return resultArray;
+}
+
+- (instancetype)initWithJson:(NSDictionary*)json {
+    self = [super init];
+    if (self) {
+        /// Заполняем поля текущего класса
+        [self getValuesForObject:self class:[self class] withJson:json];
+        
+        /// Получаем суперкласс текущего
+        Class superClass = class_getSuperclass([self class]);
+        // Заполняем поля родительских классов
+        while (superClass != [NSObject class]) {
+            Class oldClass = superClass;
+            superClass = class_getSuperclass(superClass);
+            [self getValuesForObject:self class:oldClass withJson:json];
+        }
+    }
+    
+    return self;
+}
+
+- (void)getValuesForObject:(id)object class:(Class)class withJson:(NSDictionary*)json {
+    // Получаем массив свойств текущего сласса
+    unsigned int countProperty = 0;
+    objc_property_t *property = class_copyPropertyList(class, &countProperty);
+    
+    for (unsigned int i = 0; i < countProperty; i++) {
+        // Получаем атрибут имени свойства
+        char *attributeNameProperty = property_copyAttributeValue(property[i], "V");
+        NSString *attributeNameKeyJson = [NSString stringWithFormat:@"%s", (attributeNameProperty + 1)];
+        // Получаем атрибут типа свойства
+        // @see https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/ObjCRuntimeGuide/Articles/ocrtPropertyIntrospection.html#//apple_ref/doc/uid/TP40008048-CH101
+        char *attributeTypeProperty = property_copyAttributeValue(property[i], "T");
+        
+        if (attributeTypeProperty[0] == '@') {
+            if ([json[attributeNameKeyJson] isKindOfClass:[NSDictionary class]]) {
+                NSString *className = [NSString stringWithFormat:@"%s", (attributeTypeProperty + 2)];
+                className = [className substringToIndex:className.length - 1];
+                [self setValue:[[NSClassFromString(className) alloc] initWithJson:json[attributeNameKeyJson]]
+                        forKey:[NSString stringWithFormat:@"%s", attributeNameProperty]];
+            }
+            else if ([json[attributeNameKeyJson] isKindOfClass:[NSArray class]]) {
+                NSString *className = [self classStringForField:attributeNameKeyJson];
+                if (className) {
+                    NSArray *arrayForField = [NSClassFromString(className) initArrayWithJsonArray:json[attributeNameKeyJson]];
+                    [self setValue:arrayForField forKey:[NSString stringWithFormat:@"%s", attributeNameProperty]];
+                }
+            }
+            else {
+                [self setValue:[json objectForKey:attributeNameKeyJson] forKey:[NSString stringWithFormat:@"%s", attributeNameProperty]];
+            }
+        }
+        else {
+            [self setValue:[json valueForKey:attributeNameKeyJson] forKey:[NSString stringWithFormat:@"%s", attributeNameProperty]];
+        }
+        
+        // Освобождаем память
+        free(attributeNameProperty);
+        free(attributeTypeProperty);
+    }
+    
+    // Освобождаем память
+    free(property);
+}
+
+- (NSString*)classStringForField:(NSString*)fieldName {
+    return nil;
+}
+
+#pragma mark -
+#pragma mark Description
 - (NSString *)description {
     NSMutableString *descriptionString = [NSMutableString new];
     
